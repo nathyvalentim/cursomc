5f31f0460e3fc599af9028aa969e8c0592f2b908
Nathalia Valentim <nathaliavalentim@outlook.com>
2021-02-19 21:44:16 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/livapp/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.livapp.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.livapp.cursomc.domain.Cliente;
+import com.livapp.cursomc.repositories.ClienteRepository;
+import com.livapp.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));		
+	}
+}
