d3ae871ef24ee9b757c73c854922eb99a5a0d62f
Nathalia Valentim <nathaliavalentim@outlook.com>
2021-02-24 11:17:23 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/livapp/cursomc/domain/Pagamento.java
+++ src/main/java/com/livapp/cursomc/domain/Pagamento.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.livapp.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -22,6 +23,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado; 
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId //para garantir que os id de pagamento e pedido sejam o mesmo
