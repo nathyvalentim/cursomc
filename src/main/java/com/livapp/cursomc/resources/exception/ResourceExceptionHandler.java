a537b5471dd59d34ee4e0ea92a6fb6ef49ce87ad
Nathalia Valentim <nathaliavalentim@outlook.com>
2021-03-02 13:16:51 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/livapp/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/livapp/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.livapp.cursomc.services.exceptions.DataIntegrityException;
 import com.livapp.cursomc.services.exceptions.ObjectNotFoundException; 
 
 //O Handler intercepta as exceções (um manipulador) que lança a resposta HTTP adequada
@@ -20,5 +21,14 @@ public class ResourceExceptionHandler{
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntregity(DataIntegrityException e, HttpServletRequest request){
+		
+		//Isso é assinatura padrão do ControllerAdvice
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
