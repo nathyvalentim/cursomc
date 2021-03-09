5784562367e4ba16a420ebfc797cd6f6217521b3
Nathalia Valentim <nathaliavalentim@outlook.com>
2021-03-09 20:42:32 -0300

    Validacao sintatica co Bean Validation

--- src/main/java/com/livapp/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/livapp/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,20 @@ package com.livapp.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.livapp.cursomc.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=50, message="O tamanho deve ser entre 5 e 50 caractares")
 	private String nome;
 	
 	public CategoriaDTO() {
